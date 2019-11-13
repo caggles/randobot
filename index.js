39e215afe264b2fa2a9378fb31d957c548fa6d15
Cailey Jones <cailey.jones@gov.bc.ca>
2019-11-13 14:05:04 -0800

    set up repo layout

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 require('dotenv').config()
+const fs = require('fs')
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+fs.readdir('./events/', (err, files) => {})
+
+fs.readdir('./events/', (err, files) => {
+  files.forEach(file => {
+    const eventHandler = require(`./events/${file}`)
+  })
 })
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!')
-  }
+fs.readdir('./events/', (err, files) => {
+  files.forEach(file => {
+    const eventHandler = require(`./events/${file}`)
+    const eventName = file.split('.')[0]
+    client.on(eventName, (...args) => eventHandler(client, ...args))
+  })
 })
 
 client.login(process.env.BOT_TOKEN)
