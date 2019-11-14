85412aed552981871c75c0d6b718d56b098ef8c5
caggles <caggles@users.noreply.github.com>
2019-11-13 21:27:46 -0800

    added tarot to help

--- commands/help.js
+++ commands/help.js
@@ -5,6 +5,8 @@ module.exports = message => {
     msgtext += '`!roll X` to roll a standard pool of X dice.\n'
     msgtext += '`!roll X Yagain` or `!roll X Ya` to roll a pool of X dice with Y-agains.\n'
     msgtext += 'add `$message` to the end of your roll to include the purpose or pool for your roll.\n'
-    msgtext += 'example: !roll 5 9a $wits+comp` to roll 5 dice with 9-agains for a wits and composure roll.'
+    msgtext += 'example: !roll 5 9a $wits+comp` to roll 5 dice with 9-agains for a wits and composure roll.\n'
+    msgtext += '\n**Drawing Tarot Cards**\n'
+    msgtext += '`!tarot X` to draw X tarot cards, with both upright and reverse options.'
     message.reply(msgtext)
 }
