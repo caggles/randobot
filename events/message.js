ce276fde87049271cf62b4992f3296105cbcd9e8
Cailey Jones <cailey.jones@gov.bc.ca>
2019-12-02 16:58:29 -0800

    starting commando work

--- events/message.js
+++ events/message.js
@@ -1,25 +1,25 @@
-const roll_dice = require('../commands/roll_dice')
+const roll_dice = require('../commands/dice/roll_dice')
 const help = require('../commands/help')
 const tarot_draw = require('../commands/tarot_draw')
 const tarot_lookup = require('../commands/tarot_lookup')
 
-module.exports = (client, message) => {
-  let msglist = message.content.split(" ")
-
-  if (msglist[0] === '!roll') {
-    return roll_dice(message)
-  }
-
-  if (msglist[0] === '!tarot') {
-    if (msglist[1] === 'draw') {
-      return tarot_draw(message)
-    }
-    else if (msglist[1] === 'lookup') {
-      return tarot_lookup(message)
-    }
-  }
-
-  if (message.content === '!help') {
-    return help(message)
-  }
-}
+// module.exports = (client, message) => {
+//   let msglist = message.content.split(" ")
+//
+//   if (msglist[0] === '!roll') {
+//     return roll_dice(message)
+//   }
+//
+//   if (msglist[0] === '!tarot') {
+//     if (msglist[1] === 'draw') {
+//       return tarot_draw(message)
+//     }
+//     else if (msglist[1] === 'lookup') {
+//       return tarot_lookup(message)
+//     }
+//   }
+//
+//   if (message.content === '!help') {
+//     return help(message)
+//   }
+// }
--- index.js
+++ index.js
@@ -1,22 +1,52 @@
 require('dotenv').config()
 const fs = require('fs')
 const Discord = require('discord.js')
-const client = new Discord.Client()
+//const client = new Discord.Client()
+const Commando = require('discord.js-commando');
+const path = require('path');
+const sqlite = require('sqlite');
 
-fs.readdir('./events/', (err, files) => {})
+const client = new Commando.Client({
+    commandPrefix: '!',
+    owner: '99536096764899328'
+});
 
-fs.readdir('./events/', (err, files) => {
-  files.forEach(file => {
-    const eventHandler = require(`./events/${file}`)
-  })
-})
+client.registry
+    // Registers your custom command groups
+    .registerGroups([
+        ['dice', 'Dice Commands'],
+        ['tarot', 'Tarot Commands']
+    ])
 
-fs.readdir('./events/', (err, files) => {
-  files.forEach(file => {
-    const eventHandler = require(`./events/${file}`)
-    const eventName = file.split('.')[0]
-    client.on(eventName, (...args) => eventHandler(client, ...args))
-  })
-})
+    // Registers all built-in groups, commands, and argument types
+    .registerDefaults()
 
-client.login(process.env.BOT_TOKEN)
+    // Registers all of your commands in the ./commands/ directory
+    .registerCommandsIn(path.join(__dirname, 'commands'));
+
+client.setProvider(
+    sqlite.open(path.join(__dirname, 'settings.sqlite3')).then(db => new Commando.SQLiteProvider(db))
+).catch(console.error);
+
+// fs.readdir('./events/', (err, files) => {})
+//
+// fs.readdir('./events/', (err, files) => {
+//   files.forEach(file => {
+//     const eventHandler = require(`./events/${file}`)
+//   })
+// })
+//
+// fs.readdir('./events/', (err, files) => {
+//   files.forEach(file => {
+//     const eventHandler = require(`./events/${file}`)
+//     const eventName = file.split('.')[0]
+//     client.on(eventName, (...args) => eventHandler(client, ...args))
+//   })
+// })
+
+module.exports = client => {
+  console.log(`Logged in as ${client.user.tag}!`)
+   client.user.setActivity('!help')
+}
+
+client.login(process.env.BOT_TOKEN);
